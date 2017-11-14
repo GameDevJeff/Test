ef0312ad33cfc4c2272447eebe982c4a2bb8a75a
GameDevJeff <GameDevJeff@gmail.com>
2017-11-13 17:46:43 -0800

    Commit test code

---
+++ Test/Test.cpp
@@ -0,0 +1,16 @@
+// Test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+
+	unsigned char var = 73 ^ 21;
+	std::cout << (int)var;
+
+    return 0;
+}
+
